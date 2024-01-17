98d70234f8d177f80978c37d7033c7e32015a4f0
diyantonchev-vsg <DiyTonchev@jhacorp.com>
2024-01-17 15:10:37 +0200

    feat: Made create-post.tsx a client component

--- src/app/posts/_components/create-post.tsx
+++ src/app/posts/_components/create-post.tsx
@@ -1,69 +1,46 @@
+'use client';
+
 import { createPostAction } from '../actions';
 
+import { useFormState } from 'react-dom';
+import { useRef, type ElementRef, useEffect } from 'react';
+import toast from 'react-hot-toast';
+
+import SubmitButton from './submit-button';
+
+const initialState = {
+  success: true,
+  message: ''
+};
+
 export default function CreatePost() {
+  const [state, formAction] = useFormState(createPostAction, initialState);
+
+  const inputRef = useRef<ElementRef<'input'>>(null);
+
+  useEffect(() => {
+    if (!state.success) {
+      toast(state.message)
+    }
+  }, [state]);
+
   return (
-    <form action={createPostAction}>
+    <form action={(formData: FormData) => {
+      formAction(formData);
+      inputRef.current && (inputRef.current.value = '');
+    }}>
       <input
+        ref={inputRef}
         type='text'
-        autoComplete='off'
+        autoComplete="off"
         name='post-name'
         className='p-4 text-xl text-black rounded'
         required
       />
-      <button
-        type='submit'
-        className='ml-4 rounded-xl border bg-white p-4 text-black'
-        >
-        Submit
-      </button>
+      <SubmitButton />
+      <p aria-live="polite" className="sr-only">
+        {state?.message}
+      </p>
     </form>
   );
 }
-
-
-
-
-
-
-
-
-
-// import { useFormState } from 'react-dom';
-// import { useRef, type ElementRef, useEffect } from 'react';
-// import toast from 'react-hot-toast';
-
-// import SubmitButton from './submit-button';
-
-// const initialState = {
-//   success: true,
-//   message: ''
-// };
-
-// export default function CreatePost() {
-//   const [state, formAction] = useFormState(createPostAction, initialState);
-
-//   const inputRef = useRef<ElementRef<'input'>>(null);
-
-//   useEffect(() => {
-//     if (!state.success) {
-//       toast(state.message)
-//     }
-//   }, [state]);
-  
-//   return (
-//     <form action={formAction}>
-//       <input
-//         ref={inputRef}
-//         type='text'
-//         autoComplete="off"
-//         name='post-name'
-//         className='p-4 text-xl text-black rounded'
-//         required
-//       />
-//       <SubmitButton />
-//       <p aria-live="polite" className="sr-only">
-//         {state?.message}
-//       </p>
-//     </form>
-//   );
-// }
--- src/app/posts/actions.ts
+++ src/app/posts/actions.ts
@@ -5,39 +5,18 @@ import { posts } from '~/server/db/schema';
 import { revalidatePath } from 'next/cache'
 import { z } from 'zod';
 
-export async function createPostAction(formData: FormData) {
+export async function createPostAction(prevState: { message: string, success: boolean }, formData: FormData) {
   const postNameSchema = z.string().min(1).max(256, 'The post is toooo long');
 
   const postName = formData.get('post-name'); // Get post name from formData
-  const validatedName = postNameSchema.parse(postName);
-  await db.insert(posts).values({ name: validatedName }); // Insert into DB
+
+  try {
+    const validatedName = postNameSchema.parse(postName);
+    await db.insert(posts).values({ name: validatedName }); // Insert into DB
   
-  revalidatePath('/posts'); // Revalidate page to see new content
+    revalidatePath('/posts'); // Revalidate page to see new content
+    return { success: true, message: 'Successfully added a new post' };
+  } catch (err) {
+    return { success: false, message: 'Failed to add post' };
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// export async function createPostAction(prevState: { message: string, success: boolean }, formData: FormData) {
-//   const postNameSchema = z.string().min(1).max(256, 'The post is toooo long');
-
-//   const postName = formData.get('post-name'); // Get post name from formData
-
-//   try {
-//     const validatedName = postNameSchema.parse(postName);
-//     await db.insert(posts).values({ name: validatedName }); // Insert into DB
-  
-//     revalidatePath('/posts'); // Revalidate page to see new content
-//     return { success: true, message: 'Successfully added a new post' };
-//   } catch (err) {
-//     return { success: false, message: 'Failed to add post' };
-//   }
-// }
